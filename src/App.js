eda2009708411a9f82428e7be12b0251162fc518
rahmanapyrr <rahmana.muhammad@bison.howard.edu>
2020-11-09 15:37:22 -0500

    Adding the Hangman Componenet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Hangman from './components/Hangman';
+import {useSpring, animated} from 'react-spring'
 
 function App() {
+  const props = useSpring({opacity: 1, from: {opacity: 0},Duration:200})
   return (
     <div className="App">
+      <Hangman/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Word of Fortune! Let's Play!
+
+        <p style={props}> Welcome to Word of Fortune! Let's Play!
+        <animated.div style={props}>I will fade in</animated.div>
         </p>
         <a
           className="App-link"
---
+++ src/components/Hangman.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Hangman.css';
+
+class Hangman extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hangman</h1>
+            </div>
+        )
+    }
+}
+
+export default Hangman;
