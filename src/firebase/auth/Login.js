4af8758dabcadbc52027eb1195b376b2ac161590
Payton Dennis <dennispayton15@gmail.com>
2020-11-19 19:14:19 -0600

    Homepage LogIn and Logout

--- src/firebase/auth/Login.js
+++ src/firebase/auth/Login.js
@@ -1,1 +1,20 @@
 /** Logs the user in */
+
+import { firebase, auth } from '../firebase.js';
+
+// Log the user in 
+async function logIn() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    try {
+        const result = await auth.signInWithPopup(provider);
+        const user = result.user;
+        console.log(user);
+        return user;
+    } catch (error) {
+        console.log(error.message);
+        return null;
+    }
+}
+
+export default logIn;
--- src/firebase/auth/Logout.js
+++ src/firebase/auth/Logout.js
@@ -1,1 +1,14 @@
 /** Logs the user out. */
+
+import { auth } from '../firebase.js';
+
+// Log the user out
+function logOut() {
+    try {
+        auth.signOut();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default logOut;
